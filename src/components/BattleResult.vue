002111a0efaffd80a597e2580401aeb4b557a720
jaejinlee1008 <ghkfuddks@naver.com>
2023-03-23 21:34:37 +0900

    배틀페이지 마이페이지버튼추가_2023/03/23_재진

--- src/components/BattleResult.vue
+++ src/components/BattleResult.vue
@@ -5,6 +5,7 @@
 		<h2>스크롤하여 대결 로그를 확인해보세요</h2>
 		<div v-html="battleLog" style="overflow:scroll; width:700px; height:400px;"></div>
 		<br><br>
+		<button class="btn-primary" @click="goMyPage()">MainPage로 이동</button>
 	</div>
 </template>
 
@@ -16,6 +17,11 @@ export default {
 		battleLog() {
 			return this.$store.state.battleloginfo.blog.replaceAll("\n", "<br/>")
 		}
+	},
+	methods:{
+		goMyPage() {
+            this.$router.push({path:'mypage'});
+        }
 	}
 }
 
@@ -26,6 +32,23 @@ export default {
 <style scoped>
 div.battleresult {
 	  text-align: center;
+}
+.btn-primary {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 12px 24px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 18px;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+  letter-spacing: 1px;
+  border-radius: 4px;
+  cursor: pointer;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+  transition: box-shadow 0.2s;
 }
 </style>
   
